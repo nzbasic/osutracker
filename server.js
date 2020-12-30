45b59073bb2599872b31aa25c57b7e3fdee8c9be
nzbasic <jcop589@aucklanduni.ac.nz>
2020-12-30 14:18:51 +1300

    add dotenv import

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
+require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 8080; // Step 1
