ca0b491ed32cb26bc5cd227c6ac1b2f8a8fc6ebe
nzbasic <jcop589@aucklanduni.ac.nz>
2020-12-31 23:02:02 +1300

    style name buttons to same colour as header

--- client/src/components/atoms/Name.js
+++ client/src/components/atoms/Name.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import "../../css/Material.css";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: '#3B82F6'
+  },
+});
 
 export default function Name(props) {
+  const classes = useStyles()
   return (
     <div>
       <Button
+        className={classes.root}
         id="button"
         variant="contained"
-        color="primary"
         href={"/user/" + props.name}
       >
         {props.name}
