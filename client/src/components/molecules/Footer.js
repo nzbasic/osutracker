1ca56291d10a883d60e079aa11284c98bb112733
nzbasic <jcop589@aucklanduni.ac.nz>
2021-01-03 17:04:01 +1300

    hide footer when vertical height is too small

--- client/src/components/molecules/Footer.js
+++ client/src/components/molecules/Footer.js
@@ -1,12 +1,13 @@
 import React from "react";
+import '../../css/Footer.css'
 
 export default function Footer() {
   return (
-      <div >
+      <div className="main">
         <div className="invisible lg:visible">
             <img alt="failed to load" src="https://cdn.discordapp.com/attachments/627267590862929961/794149886026055680/wave.png" width="100%" height="20%"></img>
         </div>
-        <div className="absolute bottom-10 ">
+        <div className="absolute bottom-7 ">
             <div className="flex flex-row w-screen justify-center">
                 <h1 className="py-1 px-4">Website by nzbasic</h1>
                 <a className="flex flex-row" href="https://github.com/nzbasic/osutracker" target="_blank" rel="noreferrer">
