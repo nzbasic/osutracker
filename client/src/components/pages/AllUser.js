aa93c66c41dd2219fefc95adbdf206b66d90ff29
nzbasic <jcop589@aucklanduni.ac.nz>
2021-01-13 23:28:06 +1300

    add table to all user page

--- client/src/components/pages/AllUser.js
+++ client/src/components/pages/AllUser.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../molecules/Header'
+import UserTable from '../molecules/UserTable'
 
 export default function AllUser() {
     return (
         <div className="h-screen bg-main-two">
             <Header />
-            <div>
-
+            <div className="py-2 flex flex-col px-2">
+                <UserTable />
             </div>
         </div>
     )
