7224583bb46e67fe8b229ea4683b878d612db654
nzbasic <jcop589@aucklanduni.ac.nz>
2021-01-13 22:25:35 +1300

    Add base component for all user page

---
+++ client/src/components/pages/AllUser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../molecules/Header'
+
+export default function AllUser() {
+    return (
+        <div className="h-screen bg-main-two">
+            <Header />
+            <div>
+
+            </div>
+        </div>
+    )
+}
