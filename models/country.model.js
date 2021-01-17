c3b625f827136ff5c56a29b8372a5f4a606e65d9
nzbasic <jcop589@aucklanduni.ac.nz>
2021-01-17 14:54:06 +1300

    add country model

---
+++ models/country.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+let countrySchema = new mongoose.Schema({
+    name: String,
+    abbreviation: String,
+    listPlayers: [
+        {
+            name: String
+        }
+    ],
+    scoreHistory: [
+        [
+            {
+                name: String,
+                id: String,
+                setId: String,
+                mods: Array,
+                pp: String,
+                missCount: String,
+                acc: Number
+            }
+        ]
+    ],
+    ppHistory: [
+        {
+            total: Number,
+            contributors: Array,
+            date: Number
+        }
+    ]
+})
+
+const Country = mongoose.model('country', countrySchema) 
+
+module.exports = Country
