6bb67a87af7128f9bc2082ef95a14ffd240c5b6d
nzbasic <jcop589@aucklanduni.ac.nz>
2021-01-17 14:54:19 +1300

    add country endpoints

---
+++ routes/country.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+let Country = require("../models/country.model");
+
+router.route("/names").get((req, res) => {
+  Country.find({}, { name: 1 })
+    .then((countries) => res.json(countries))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/data/:country").get((req, res) => {
+  Country.find({ name: req.params.country })
+    .then((country) => {
+      res.json(country);
+    })
+    .catch((err) => res.status(400).json("Error: " + err + req.params.country));
+});
+
+router.route("/ppHistory/:country").get((req, res) => {
+    Country.find({ name: req.params.country }, { ppHistory: 1 })
+        .then(history => {
+            res.json(history)
+        })
+        .catch(err => res.status(400).json("error: " + err + req.params.country))
+})
+
+router.route("/scores/:country/latest").get((req, res) => {
+    Country.find({ name: req.params.country }, { scoreHistory: 1 })
+        .then(history => {
+            res.json(history[0].scoreHistory[history[0].scoreHistory.length-1])
+        })
+        .catch(err => res.status(400).json("error: " + err + req.params.country))
+})
+
+router.route("/scores/:country/all").get((req, res) => {
+    Country.find({ name: req.params.country }, { scoreHistory: 1 })
+        .then(history => {
+            res.json(history[0].scoreHistory)
+        })
+        .catch(err => res.status(400).json("error: " + err + req.params.country))
+})
+
+module.exports = router;
