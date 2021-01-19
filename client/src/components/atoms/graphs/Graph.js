ed3fe1384bb0652c0df721e3f5bd67843d0fe29e
nzbasic <jcop589@aucklanduni.ac.nz>
2021-01-20 12:23:51 +1300

    remove canvajs graphs

--- client/src/components/atoms/graphs/Graph.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import CanvasJSReact from "../../../lib/canvasjs.react";
-
-let CanvasJS = CanvasJSReact.CanvasJS;
-let CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
-export { CanvasJS, CanvasJSChart };
-
-export default class Graph extends Component {}
