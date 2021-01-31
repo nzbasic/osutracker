518fc88133a6a10d33228396ed1b9e9d00f3ec8e
nzbasic <jcop589@aucklanduni.ac.nz>
2021-02-01 01:28:17 +1300

    fix day display

--- client/src/components/molecules/TimeSeriesChart.js
+++ client/src/components/molecules/TimeSeriesChart.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import moment from 'moment'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
 
 import {
   ResponsiveContainer,
@@ -11,49 +11,86 @@ import {
   LineChart,
   XAxis,
   YAxis,
-} from 'recharts'
+} from "recharts";
 
-const CustomTooltip = ({ active, payload, label}) => {
-    if (active) {
-        return (
-            <div className="text-main-one">
-                <p className="label">{moment(label).format("DDD M YY") + " : " + payload[0].value}</p>
-            </div>
-        )
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active) {
+    return (
+      <div className="text-main-one">
+        <p className="label">
+          {moment(label).format("DD M YY") + " : " + payload[0].value}
+        </p>
+      </div>
+    );
+  }
+  return null;
+};
+
+const TimeSeriesChart = (props) => {
+  const [zoomState, setZoomState] = useState();
+
+  // for desktop
+  const handleZoomMouseDown = (e) =>
+    e && setZoomState({ ...zoomState, refAreaLeft: e.activeLabel });
+
+  const handleZoomMouseMove = (e) =>
+    e &&
+    zoomState.refAreaLeft &&
+    setZoomState({ ...zoomState, refAreaRight: e.activeLabel });
+
+  const handleZoomMouseUp = (e) => {
+    if (
+      !(e && zoomState.refAreaLeft && zoomState.refAreaRight) ||
+      zoomState.refAreaLeft === zoomState.refAreaRight
+    ) {
+      setZoomState();
+      return;
     }
-    return null
-}
 
-const TimeSeriesChart = (props) => (
-  <ResponsiveContainer width='95%' height='95%' >
-    <LineChart data={props.chartData} margin={{top:25}}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis
-        dataKey='x'
-        domain={['auto', 'auto']}
-        name='Date'
-        tickFormatter={(unixTime) => moment(unixTime).format('HH:mm Do')}
-        type='number'
-      />
-      <YAxis dataKey='y' name='pp' domain={['dataMin-0.1', 'auto']} />
-      <Tooltip content={<CustomTooltip />}/>
-      <Line
-        strokeWidth={2}
-        dataKey="y"
-        type="monotone"
-        stroke="#c91a34"
-      />
-    </LineChart>
-  </ResponsiveContainer>
-)
+    const min = Math.min(zoomState.refAreaRight, zoomState.refAreaLeft);
+    const max = Math.max(zoomState.refAreaRight, zoomState.refAreaLeft);
+
+    setZoomState({
+      ...zoomState,
+      xMin: min,
+      xMax: max,
+      refAreaLeft: undefined,
+      refAreaRight: undefined,
+    });
+  };
+
+  return (
+    <ResponsiveContainer width="95%" height="95%">
+      <LineChart 
+        data={props.chartData} 
+        margin={{ top: 25 }}
+        onMouseDown={e => handleZoomMouseDown}
+        onMouseMove={e => handleZoomMouseMove}
+        onMouseUp={e => handleZoomMouseUp}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="x"
+          domain={["auto", "auto"]}
+          name="Date"
+          tickFormatter={(unixTime) => moment(unixTime).format("HH:mm Do")}
+          type="number"
+        />
+        <YAxis dataKey="y" name="pp" domain={["dataMin-0.1", "auto"]} />
+        <Tooltip content={<CustomTooltip />} />
+        <Line strokeWidth={2} dataKey="y" type="monotone" stroke="#c91a34" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
 
 TimeSeriesChart.propTypes = {
   chartData: PropTypes.arrayOf(
     PropTypes.shape({
       time: PropTypes.number,
-      value: PropTypes.number
+      value: PropTypes.number,
     })
-  ).isRequired
-}
+  ).isRequired,
+};
 
-export default TimeSeriesChart
+export default TimeSeriesChart;
