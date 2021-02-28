f459bc2a2c19d9514fc22ad9aafb78fabfc042dc
nzbasic <jcop589@aucklanduni.ac.nz>
2021-02-28 14:54:19 +1300

    show added plays on each timestamp

--- client/src/components/molecules/UserPlays.js
+++ client/src/components/molecules/UserPlays.js
@@ -5,14 +5,55 @@ import 'animate.css'
 
 export default function UserPlays({ plays, currentTop, country }) {
 
-  const [date, setDate] = useState(new Date().getTime())
-  const [currentIndex, setCurrentIndex] = useState(plays.length)
-  const [currentPlays, setCurrentPlays] = useState(currentTop)
+  const [playsHistory, setPlaysHistory] = useState([{scores: currentTop, date: new Date().getTime()}])
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [isLoading, setLoading] = useState(true)
+
+  console.log(plays)
+
+  useEffect(() => {
+    if (plays.length != 0) {
+      
+      for (let i=plays.length-1; i>=0; i--) {
+        
+  
+        let lastPlays = clonedeep(playsHistory[0].scores) 
+        console.log(lastPlays)
+        let change = plays[i]
+        let added = change.added
+        let removed = change.removed
+  
+        let toRemove = []
+  
+        lastPlays.forEach(play1 => {
+          toRemove.push(added.findIndex(play2 => (play1.id == play2.id) && (play1.pp == play2.pp)))
+        })
+  
+        toRemove.forEach((value, index) => {
+          if (value > -1) {
+            playsHistory[0].scores[index].added = true
+            lastPlays.splice(index, 1)
+          }
+        })
+  
+        removed.forEach(play => {
+          lastPlays.push(play)
+        })
+  
+        lastPlays.sort((play1, play2) => parseInt(play2.pp) - parseInt(play1.pp))
+  
+        playsHistory.unshift({scores: lastPlays, date: plays[i].date})
+      }
+      setPlaysHistory(playsHistory)
+      setCurrentIndex(plays.length)
+      setLoading(false)
+    }
+  },[])
 
   const Play = ({ data, index }) => (
 
     <ScrollAnimation animateIn="animate__slideInRight" duration={0.3} animateOnce>
-    <div className={`bg-main-one flex rounded-md shadow-md p-2 my-2 justify-between w-full text-xs md:text-sm lg:text-base`}>
+    <div className={`${data.added ? "bg-green-500 text-main-one" : data.removed ? "bg-red-600 text-main-one" : "bg-main-one"} flex rounded-md shadow-md p-2 my-2 justify-between w-full text-xs md:text-sm lg:text-base`}>
       <div className="flex">
           {country ? (<div className="self-center mr-2 hidden md:block lg:hidden md:w-20">{preventOverflow(data.player, 9)}</div>): ""}
           {country ? (<div className="self-center mr-2 hidden lg:block lg:w-32">{preventOverflow(data.player, 10)}</div>): ""}
@@ -38,78 +79,15 @@ export default function UserPlays({ plays, currentTop, country }) {
     
   );
 
-  const goBack = () => {
-    let nextPlays = plays[currentIndex-1]
-    let added = nextPlays.added
-    let removed = nextPlays.removed
-    setCurrentIndex(currentIndex-1)
-    setDate(nextPlays.date)
-    
-    let current = clonedeep(currentPlays)
-
-    let toRemove = []
-    current.forEach(play1 => {
-      toRemove.push(added.findIndex(play2 => (play1.id == play2.id) && (play1.pp == play2.pp)))
-    })
-
-    toRemove.forEach((value, index) => {
-      if (value > -1) current.splice(index, 1)
-    })
-
-    removed.forEach(play => {
-      if(currentIndex-1 > 0) {
-        play.added = true
-      }
-      current.push(play)
-    })
-    
-    current.sort((play1, play2) => parseInt(play2.pp) - parseInt(play1.pp))
-    setCurrentPlays(current)
-  }
-
-  const goForward = () => {
-
-    let nextPlays = plays[currentIndex] 
-    let added = nextPlays.added
-    let removed = nextPlays.removed
-
-    setCurrentIndex(currentIndex+1)
-
-    if ((currentIndex+1) == plays.length) {
-      setDate(new Date().getTime())
-    } else {
-      setDate(nextPlays.date)
-    }
-
-    let current = clonedeep(currentPlays)
-
-    let toRemove = []
-    current.forEach(play1 => {
-      toRemove.push(removed.findIndex(play2 => (play1.id == play2.id) && (play1.pp == play2.pp)))
-    })
-
-    toRemove.forEach((value, index) => {
-      if (value > -1) current.splice(index, 1)
-    })
-
-    added.forEach(play => {
-      current.push(play)
-    })
-
-    current.sort((play1, play2) => parseInt(play2.pp) - parseInt(play1.pp))
-    setCurrentPlays(current)
-  }
-
   return (
     <div className="w-full max-w-full flex flex-col">
-
       <div className="self-center p-2 mb-4 bg-main-one rounded-md shadow-md flex">
-        <div className={`${currentIndex > 0 ? "block" : "invisible"} mr-2 hover:text-main-four font-extrabold cursor-pointer`} onClick={goBack}>⟵</div>
-        <div className="font-semibold">{new Date(date).toDateString()}</div>
-        <div className={`${currentIndex == plays.length ? "invisible" : "block"} ml-2 hover:text-main-four font-extrabold cursor-pointer`} onClick={goForward}>⟶</div>
+        <div className={`${currentIndex > 0 ? "block" : "invisible"} mr-2 hover:text-main-four font-extrabold cursor-pointer`} onClick={() => setCurrentIndex(currentIndex-1)}>⟵</div>
+        <div className="font-semibold">{new Date(playsHistory[currentIndex].date).toDateString()}</div>
+        <div className={`${currentIndex < plays.length ? "block" : "invisible"} ml-2 hover:text-main-four font-extrabold cursor-pointer`} onClick={() => setCurrentIndex(currentIndex+1)}>⟶</div>
       </div>
 
-      {currentPlays.map((play, index) => (
+      {playsHistory[currentIndex].scores.map((play, index) => (
         <Play key={index} data={play} index={index + 1} />
       ))}
     </div>
