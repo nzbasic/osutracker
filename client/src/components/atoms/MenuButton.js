b4ff5ddfd76bc77aeb03f2add501b25e67664e02
nzbasic <jcop589@aucklanduni.ac.nz>
2021-03-14 15:54:24 +1300

    add optional target prop

--- client/src/components/atoms/MenuButton.js
+++ client/src/components/atoms/MenuButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function MenuButton({ text, style, link }) {
+export default function MenuButton({ text, style, link, target }) {
   return (
     <a
       style={style}
       href={link}
+      target={target ? target : ""}
       className="py-4 text-xl lg:text-base font-semibold lg:py-0 w-full lg:w-auto text-left px-4 lg:px-2 cursor-pointer lg:hover:text-main-four hover:bg-gray-100 lg:hover:bg-white"
     >
       <div>{text}</div>
