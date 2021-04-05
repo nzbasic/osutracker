e058a77f2dd986dc3dba95acd96ffdf78ecccb00
nzbasic <jcop589@aucklanduni.ac.nz>
2021-04-05 23:07:45 +1200

    add stats router

---
+++ routes/stats.js
@@ -0,0 +1,29 @@
+import express from "express";
+import OverallStats from "../models/OverallStats.model.js";
+import Beatmap from "../models/Beatmap.model.js";
+const router = express.Router();
+
+router.route("/").get((req, res) => {
+  OverallStats.findOne({})
+    .then((stats) => res.json(stats))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/mapset/:id").get((req, res) => {
+  Beatmap.findOne({ id: req.params.id })
+    .then((map) => res.json(map))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/mapsets").get(async (req, res) => {
+  let result = [];
+
+  for (const setId of req.query.arr) {
+    let beatmap = await Beatmap.findOne({ setId: setId });
+    result.push(beatmap);
+  }
+
+  res.json(result);
+});
+
+export default router;
