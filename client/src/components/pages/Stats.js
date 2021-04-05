5d1b39a225b2609d1359182c5bd4da7a4ebb48c5
nzbasic <jcop589@aucklanduni.ac.nz>
2021-04-05 23:08:44 +1200

    add stats page

---
+++ client/src/components/pages/Stats.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { CircularProgress } from "@material-ui/core";
+import AverageUser from "../molecules/AverageUser.js";
+import AverageCountry from "../molecules/AverageCountry.js";
+import UserPlays from "../molecules/UserPlays.js";
+import TopPlay from "../molecules/TopPlay.js";
+import TopMods from "../molecules/TopMods.js";
+import TopMappers from "../molecules/TopMappers.js";
+import TopMapSets from "../molecules/TopMapSets.js";
+import Footer from "../molecules/Footer.js";
+
+export default function Stats() {
+  const [stats, setStats] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let stats = (await axios.get("/api/stats/")).data;
+      console.log(stats);
+      setStats(stats);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <div className="w-screen h-screen flex justify-center align-center">
+      <CircularProgress className="self-center" size="10rem" />
+    </div>
+  ) : (
+    <div className="flex flex-col w-full items-center">
+      <div className="inline-flex flex-col w-smgraph lg:w-3/5 items-center mt-12 lg:mt-0">
+        <div className="w-full flex flex-wrap justify-center">
+          <div className="m-2">
+            <div className="mt-4 bg-main-one shadow-md rounded-md p-2 font-bold text-center">
+              Average Tracked User
+            </div>
+            <div>
+              <AverageUser data={stats.userStats} />
+            </div>
+          </div>
+
+          <div className="h-full flex flex-col m-2">
+            <div className="mt-4 bg-main-one shadow-md rounded-md p-2 font-bold text-center">
+              Average Tracked Country
+            </div>
+            <div className="h-full">
+              <AverageCountry data={stats.countryStats} />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-2 w-full bg-main-one shadow-md rounded-md p-2 font-bold text-center">
+          Average Top Play
+        </div>
+        <div className="w-full">
+          <TopPlay id={stats.userStats.topPlay} />
+        </div>
+
+        <div className="mt-4 w-full bg-main-one shadow-md rounded-md p-2 font-bold text-center">
+          Top 10 Mods
+        </div>
+        <div className="w-full">
+          <TopMods data={stats.userStats.modsCount.slice(0, 10)} />
+        </div>
+
+        <div className="mt-4 w-full bg-main-one shadow-md rounded-md p-2 font-bold text-center">
+          Top 50 Mappers
+        </div>
+        <div className="w-full">
+          <TopMappers data={stats.mapperCount.slice(0, 50)} />
+        </div>
+
+        <div className="mt-4 w-full bg-main-one shadow-md rounded-md p-2 font-bold text-center">
+          Top 1000 Map Sets
+        </div>
+        <div className="w-full">
+          <TopMapSets data={stats.setCount.slice(0, 1000)} />
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
