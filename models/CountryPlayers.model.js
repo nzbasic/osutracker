b99d7ddcac85987ab23dc8463f4a78ee89ca2917
nzbasic <jcop589@aucklanduni.ac.nz>
2021-04-07 19:36:52 +1200

    add a mark for testing purposes

--- models/CountryPlayers.model.js
+++ models/CountryPlayers.model.js
@@ -4,6 +4,7 @@ let countryPlayersSchema = new mongoose.Schema({
   name: String,
   date: Number,
   listPlayers: Array,
+  mark: Number,
 });
 
 const CountryPlayers = mongoose.model("countryPlayers", countryPlayersSchema);
