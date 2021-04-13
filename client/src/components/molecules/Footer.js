2cdbee01ef70ffb870200d432bfe7e8bbc5bbb9a
nzbasic <jcop589@aucklanduni.ac.nz>
2021-04-13 17:30:25 +1200

    update footer

--- client/src/components/molecules/Footer.js
+++ client/src/components/molecules/Footer.js
@@ -1,14 +1,15 @@
 import React from "react";
 import GitHub from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import "../../css/Main.css";
 
 export default function Footer() {
   return (
     <div
       id="thirdDiv"
-      className=" my-4 w-full bg-main-one h-12 inline-flex flex-row rounded-lg shadow justify-center self-center"
+      className=" my-4 w-full bg-main-one h-12 inline-flex flex-row rounded-lg shadow justify-center self-center space-x-4"
     >
-      <div className="self-center px-2">Site by nzbasic</div>
+      <div className="self-center">nzbasic</div>
       <a
         href="https://github.com/nzbasic/osutracker"
         target="_blank"
@@ -16,7 +17,22 @@ export default function Footer() {
         className="flex items-center"
       >
         <GitHub fontSize="small" />
-        <div className="px-2 hover:text-main-four">View Source</div>
+      </a>
+      <a
+        href="https://twitter.com/nzbasic"
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center"
+      >
+        <TwitterIcon fontSize="small" />
+      </a>
+      <a
+        href="https://osu.ppy.sh/users/9008211"
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center hover:text-main-four cursor-pointer"
+      >
+        osu!
       </a>
     </div>
   );
