bcdc36b6b716255229af4490c6cd7af45a3f2662
nzbasic <jcop589@aucklanduni.ac.nz>
2021-04-13 20:55:42 +1200

    add links to mappers

--- client/src/components/molecules/TopMappers.js
+++ client/src/components/molecules/TopMappers.js
@@ -4,9 +4,15 @@ export default function TopMappers({ data }) {
   return (
     <div className="bg-main-one mt-4 p-2 shadow-md rounded-md flex flex-col">
       {data.map((mapper, index) => (
-        <div key={mapper.mapper}>
+        <a
+          key={mapper.mapper}
+          href={"https://osu.ppy.sh/users/" + mapper.mapper}
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-main-four"
+        >
           {index + 1 + " " + mapper.mapper + ": " + mapper.count}
-        </div>
+        </a>
       ))}
     </div>
   );
