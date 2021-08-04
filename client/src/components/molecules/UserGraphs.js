604a4e8d8dcd443fa228afc565d195878cfcf4c2
nzbasic <jcop589@aucklanduni.ac.nz>
2021-08-04 17:29:03 +1200

    replace buttons with dropdown

--- client/src/components/molecules/UserGraphs.js
+++ client/src/components/molecules/UserGraphs.js
@@ -1,52 +1,75 @@
 import React, { useState, useEffect } from "react";
 import TimeSeriesChart from "./TimeSeriesChart.js";
+import GraphDropdown from "../molecules/GraphDropdown.js";
 
 export default function UserGraphs({ data }) {
-  const [buttonIndex, setButtonIndex] = useState(0);
+  const [graphType, setGraphType] = useState("pp");
+  const [reversed, setReversed] = useState(false);
+
   const [ppPoints, setPpPoints] = useState([]);
   const [accPoints, setAccPoints] = useState([]);
   const [playPoints, setPlayPoints] = useState([]);
   const [rankPoints, setRankPoints] = useState([]);
   const [farmPoints, setFarmPoints] = useState([]);
   const [rangePoints, setRangePoints] = useState([]);
+  const [scorePoints, setScorePoints] = useState([]);
+  const [countryRankPoints, setCountryRankPoints] = useState([]);
 
   useEffect(() => {
-    let pp = [];
-    let acc = [];
-    let play = [];
-    let rank = [];
-    let farm = [];
-    let range = [];
+    const pp = [];
+    const acc = [];
+    const play = [];
+    const rank = [];
+    const farm = [];
+    const range = [];
+    const score = [];
+    const countryRank = [];
 
     for (let i = 0; i < data.length; i++) {
+      const date = data[i].date;
+
       pp.push({
-        x: data[i].date,
+        x: date,
         y: parseInt(data[i].pp),
       });
       rank.push({
-        x: data[i].date,
+        x: date,
         y: parseInt(data[i].rank),
       });
       acc.push({
-        x: data[i].date,
+        x: date,
         y: Math.round((parseFloat(data[i].acc) + Number.EPSILON) * 100) / 100,
       });
       play.push({
-        x: data[i].date,
+        x: date,
         y: parseInt(data[i].plays),
       });
 
       if (data[i].farm) {
         farm.push({
-          x: data[i].date,
+          x: date,
           y: data[i].farm,
         });
       }
 
       if (data[i].range) {
         range.push({
-          x: data[i].date,
+          x: date,
           y: parseInt(data[i].range),
+        });
+      }
+
+      if (data[i].score) {
+        score.push({
+          x: date,
+          y: parseInt(data[i].score),
+        });
+      }
+
+      if (data[i].countryRank) {
+        countryRank.push({
+          x: date,
+          y: parseInt(data[i].countryRank),
         });
       }
     }
@@ -57,53 +80,61 @@ export default function UserGraphs({ data }) {
     setPlayPoints(play.sort((a, b) => a.x - b.x));
     setFarmPoints(farm.sort((a, b) => a.x - b.x));
     setRangePoints(range.sort((a, b) => a.x - b.x));
+    setScorePoints(score.sort((a, b) => a.x - b.x));
+    setCountryRankPoints(countryRank.sort((a, b) => a.x - b.x));
   }, [data]);
 
-  const arr = ["Pp", "Rank", "Acc", "Plays", "Farm", "Range"];
+  const graphChange = (e) => {
+    setGraphType(e.value);
+    setReversed(e.reversed);
+  };
 
   return (
     <div className="inline-flex flex-col items-center py-2">
-      <div className="flex space-x-1 bg-main-one p-2 rounded-md shadow-lg">
-        {arr.map((name, index) => (
-          <GraphButton
-            key={name}
-            text={name}
-            onClick={() => setButtonIndex(index)}
-            active={buttonIndex === index}
-          />
-        ))}
+      <div className="">
+        <GraphDropdown onChange={graphChange} />
       </div>
       <div className="inline-flex justify-center">
         <div className="">
           <div className="bg-main-one inline-flex rounded-md pt-2 lg:w-graph w-smgraph md:px-2 my-4 h-96 shadow-lg">
             <ToggleGraph
               data={ppPoints}
-              active={buttonIndex === 0}
+              active={graphType === "pp"}
               reversed={false}
             />
             <ToggleGraph
               data={rankPoints}
-              active={buttonIndex === 1}
+              active={graphType === "rank"}
               reversed={true}
             />
             <ToggleGraph
               data={accPoints}
-              active={buttonIndex === 2}
+              active={graphType === "acc"}
               reversed={false}
             />
             <ToggleGraph
               data={playPoints}
-              active={buttonIndex === 3}
+              active={graphType === "plays"}
               reversed={false}
             />
             <ToggleGraph
               data={farmPoints}
-              active={buttonIndex === 4}
+              active={graphType === "farm"}
               reversed={false}
             />
             <ToggleGraph
               data={rangePoints}
-              active={buttonIndex === 5}
+              active={graphType === "range"}
+              reversed={false}
+            />
+            <ToggleGraph
+              data={scorePoints}
+              active={graphType === "score"}
+              reversed={false}
+            />
+            <ToggleGraph
+              data={countryRankPoints}
+              active={graphType === "countryRank"}
               reversed={false}
             />
           </div>
@@ -112,17 +143,6 @@ export default function UserGraphs({ data }) {
     </div>
   );
 }
-
-const GraphButton = ({ text, onClick, active }) => (
-  <div
-    className={`${
-      active ? "bg-main-four" : "hover:bg-gray-400 cursor-pointer"
-    } lg:text-2xl font-semibold shadow outline-inner bg-main-one p-1 text-xs md:text-base w-10 md:w-20 lg:w-32 text-center `}
-    onClick={onClick}
-  >
-    {text}
-  </div>
-);
 
 const ToggleGraph = ({ active, data, reversed }) => (
   <div className={`${active ? "block" : "hidden"} w-full h-full`}>
