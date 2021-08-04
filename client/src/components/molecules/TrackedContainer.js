4b6202a5d0933f21ce68ea477acb1cd1613ca1a7
nzbasic <jcop589@aucklanduni.ac.nz>
2021-08-04 17:12:14 +1200

    add more props to search

--- client/src/components/molecules/TrackedContainer.js
+++ client/src/components/molecules/TrackedContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Tracked from "../atoms/Tracked.js";
 
-export default function TrackedContainer({ items }) {
+export default function TrackedContainer({ items, open, select, item }) {
   return (
     <div className="w-full flex p-2 flex-col items-center bg-main-one rounded-md space-y-1 shadow">
       {items.map((name) => (
-        <Tracked name={name} key={name.name} />
+        <Tracked
+          name={name}
+          key={name.name}
+          open={open}
+          select={select}
+          item={item}
+        />
       ))}
       {items.length === 0 ? "No Results" : null}
     </div>
