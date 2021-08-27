892cf33ff0668ca348058e0b2598fd342931ee69
nzbasic <jcop589@aucklanduni.ac.nz>
2021-08-28 10:52:19 +1200

    SEO

--- client/src/components/atoms/MenuButton.js
+++ client/src/components/atoms/MenuButton.js
@@ -8,7 +8,7 @@ export default function MenuButton({ text, style, link, target }) {
       target={target ? target : ""}
       className="py-4 text-xl lg:text-base font-semibold lg:py-0 w-full lg:w-auto text-left px-4 lg:px-2 cursor-pointer lg:hover:text-main-four hover:bg-gray-100 lg:hover:bg-white"
     >
-      <div>{text}</div>
+      {text}
     </a>
   );
 }
