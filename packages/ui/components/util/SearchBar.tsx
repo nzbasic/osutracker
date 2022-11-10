d4b76728c63c7560d81f4ed5d8e892e02a789926
James <54062686+nzbasic@users.noreply.github.com>
2022-11-10 18:08:12 +1300

    Add styles to search bar

--- packages/ui/components/util/SearchBar.tsx
+++ packages/ui/components/util/SearchBar.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import classNames from 'classnames';
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 
 interface User {
   name: string;
@@ -16,9 +19,16 @@ interface SearchBarProps<T extends keyof Registry> {
 
 export const SearchBar = <T extends keyof Registry>({ className, children, query }: SearchBarProps<T>) => {
   return (
-    <input 
-      className={classNames(className, "p-2 bg-white rounded-md border text-black")} 
-      placeholder="Search..."
-    />
+    <div className="bg-white relative pointer-events-auto">
+      <button 
+        className={classNames(
+          className, 
+          "hidden w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-300"
+        )} 
+      >
+        <MagnifyingGlassIcon className="mr-3 flex-none w-6 h-6" />
+        Quick search...
+      </button>
+    </div>
   );
 };
