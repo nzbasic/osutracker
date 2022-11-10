944e38990cf7d521a7b0bf840edc5251403e0ac1
James <54062686+nzbasic@users.noreply.github.com>
2022-11-10 18:08:27 +1300

    Add animation to dropdown

--- packages/ui/components/layout/app/sidebar/Button.tsx
+++ packages/ui/components/layout/app/sidebar/Button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronUpIcon } from '@heroicons/react/20/solid';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import classNames from 'classnames';
 import Link from 'next/link';
 
@@ -22,8 +22,8 @@ const Button = ({ setActive, link, isActive, Icon, title, hasChildren }: ButtonP
       href={link}
       className={classNames(
         { "bg-gray-100 text-black": isActive },
-        { "bg-white text-gray-500": !isActive },
-        "rounded font-semibold transition-colors hover:bg-blue-500 hover:text-white",
+        { "bg-white text-gray-500 hover:bg-blue-500 hover:text-white": !isActive },
+        "rounded font-semibold transition-colors",
         "flex gap-4 p-2"
       )}
     >
@@ -31,7 +31,7 @@ const Button = ({ setActive, link, isActive, Icon, title, hasChildren }: ButtonP
       {title}
       {hasChildren && (
         <div className="flex-grow">
-          <ChevronUpIcon
+          <ChevronDownIcon
             className={classNames("ml-auto transition-all", { 'rotate-180': isActive })}
             width={iconSize}
             height={iconSize}
--- packages/ui/components/layout/app/sidebar/SidebarItem.tsx
+++ packages/ui/components/layout/app/sidebar/SidebarItem.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Button from "./Button";
 import { MenuItem } from "./items";
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface SidebarItemProps {
   title: string;
@@ -12,6 +13,9 @@ interface SidebarItemProps {
   currentActive: string;
   setActive: React.Dispatch<React.SetStateAction<string>>;
 }
+
+const itemDuration = 0.2;
+const itemDelay = 0.2;
 
 export const SidebarItem = ({
   title,
@@ -23,8 +27,13 @@ export const SidebarItem = ({
 }: SidebarItemProps) => {
   const isActive = React.useMemo(() => currentActive.startsWith(link), [currentActive, link])
 
+  const parentDuration = React.useMemo(() => {
+    if (!subItems) return 0;
+    return subItems.length * (itemDelay) - itemDuration
+  }, [subItems])
+
   return (
-    <div>
+    <div className="flex flex-col">
       <Button 
         hasChildren
         setActive={setActive} 
@@ -33,22 +42,45 @@ export const SidebarItem = ({
         Icon={Icon} 
         title={title}        
       />
-      {subItems && isActive && subItems.length > 0 && (
-        <div className="ml-2 mt-2 flex gap-2">
-          <span className="border-l-2 border-zinc-200" />
-          <div className="flex flex-grow flex-col gap-2">
-            {subItems.map((subItem) => (
-              <Button
-                title={subItem.title}
-                Icon={subItem.Icon}
-                link={subItem.link}
-                isActive={currentActive.endsWith(subItem.link)}
-                setActive={setActive}
-              />
-            ))}
-          </div>
-        </div>
-      )}
+
+      <AnimatePresence>
+        {subItems && isActive && subItems.length > 0 && (
+          <motion.div 
+            initial={{ height: 0 }}
+            animate={{ height: 'auto' }}
+            transition={{ duration: parentDuration, delay: itemDelay / 2 }}
+            exit={{ height: 0, transition: { duration: parentDuration + itemDuration }}}
+            className="ml-2 flex gap-2"
+          >
+            <span className="mt-2 border-l-2 border-zinc-200" />
+            <div className="mt-2 flex flex-grow flex-col gap-2">
+              {subItems.map((subItem, i) => (
+                <motion.div
+                  key={subItem.link}
+                  initial={{ scale: 0 }} 
+                  animate={{ scale: 1 }}
+                  transition={{ duration: itemDuration, delay: (i + 1) * itemDelay }}
+                  exit={{ 
+                    scale: 0, 
+                    transition: { 
+                      duration: itemDuration, 
+                      delay: (subItems.length - i - 1) * itemDelay/2
+                    } 
+                  }}
+                >
+                  <Button
+                    title={subItem.title}
+                    Icon={subItem.Icon}
+                    link={subItem.link}
+                    isActive={currentActive.endsWith(subItem.link)}
+                    setActive={setActive}
+                  />
+                </motion.div>
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
