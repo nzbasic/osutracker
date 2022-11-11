08ee4800f3ce4365b517663fe2441b68c60f1bb3
James <54062686+nzbasic@users.noreply.github.com>
2022-11-11 13:30:15 +1300

    Fix scrolling past modal

--- packages/ui/components/util/ModalWrapper.tsx
+++ packages/ui/components/util/ModalWrapper.tsx
@@ -1,15 +1,20 @@
 "use client"
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { MouseEventHandler, useEffect, useRef } from "react";
 
 interface ModalWrapperProps {
+  show: boolean;
   close: () => void;
   children: (close: () => void) => JSX.Element;
 }
 
-export const ModalWrapper = ({ close, children }: ModalWrapperProps) => {
+export const ModalWrapper = ({ show, close, children }: ModalWrapperProps) => {
   const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('overflow-hidden', show)
+  }, [show])
 
   useEffect(() => {
     const listener = (e: KeyboardEvent) => {
@@ -25,20 +30,23 @@ export const ModalWrapper = ({ close, children }: ModalWrapperProps) => {
   }
 
   return (
-    <div
-      ref={ref}
-      className="absolute z-50 top-0 left-0 h-screen w-screen bg-gray-400/30 flex items-center justify-center" 
-      onClick={handleClick}
-    >
-      <motion.div 
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        exit={{ scale: 0 }}
-        className="max-w-2xl w-full"
-      >
-        {children(close)}
-      </motion.div>
-      
-    </div>
-  )
-}
+    <AnimatePresence>
+      {show && (
+        <div
+          ref={ref}
+          className="absolute z-50 top-0 left-0 h-screen w-screen bg-gray-400/30 flex items-center justify-center" 
+          onClick={handleClick}
+        >
+          <motion.div 
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0 }}
+            className="max-w-2xl w-full"
+          >
+            {children(close)}
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>
+  );
+};
